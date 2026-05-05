b6beec5b1ac7767f11f165299669dfac01733095
the.chicken.nugget_yaaaasssss <95938458+TheChickenNuggetYaaaasssss@users.noreply.github.com>
2026-05-05 12:42:06 +0100

    Update Main.java

--- src/main/java/com/atmbanksimulator/Main.java
+++ src/main/java/com/atmbanksimulator/Main.java
@@ -41,7 +41,7 @@ public class Main extends Application {
                     e.printStackTrace();
                 }
             } else {
-                System.out.println("doesnt exist yo");
+                System.out.println("doesnt exist");
             }
         }
         //UIModel-View-Controller structure setup
